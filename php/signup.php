02995fa75458249a86e888010bbdf2c40b731b53
EnergieNVidia77 <toufictalha@gmail.com>
2021-03-18 13:02:46 +0100

    Bug fix

--- php/signup.php
+++ php/signup.php
@@ -4,12 +4,11 @@
     $usrEmail = $_POST["usrEmail"];
     $usrPassword = $_POST["usrPassword"];
 
-    $usrIDHash = password_hash($usrID, PASSWORD_DEFAULT);
     $usrEmailHash = password_hash($usrEmail, PASSWORD_DEFAULT);
     $usrPasswordHash = password_hash($usrPassword, PASSWORD_DEFAULT);
 
-    $user_log = array("usrID"=>"$usrIDHash", "usrEmail"=>"$usrEmailHash", "usrPassword"=>"$usrEmailHash");
+    $user_log = array("usrID"=>"$usrID", "usrEmail"=>"$usrEmailHash", "usrPassword"=>"$usrEmailHash");
 
-    
+
     
 ?>
