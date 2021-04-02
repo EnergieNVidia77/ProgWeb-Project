50e20a92bfb73608cf93df74462f1a794fe76961
toufic.talha <toufic.talha@u-psud.fr>
2021-04-02 21:00:10 +0200

    more bug fix

--- php/createBallot.php
+++ php/createBallot.php
@@ -1,8 +1,17 @@
 <?php
 
     $CurrentUsrID = $_COOKIE["CurrentUsrID"];
-    
-    $ballots_raw = file_get_contents("../logs/ballots.json");
+    $voteTitle = $_POST["voteTitle"];
+    $voteQuestion = $_POST["voteQuestion"];
+    $voteChoices = $_POST["arrayOfChoice"];
+    $voteID = uniqid();
+
+    $array_of_votes_raw = file_get_contents("../logs/ballots.json");
+    $array_of_votes = json_decode($array_of_votes_raw, true);
+
+    $own_registration = array("userID" => $CurrentUsrID, "vote" => "NULL", "nbVote" => "1", "procuration" => "NULL");
+
+    $vote = array(0 => array("voteID" => $voteID, "title" => $voteTitle, "promoter" => $CurrentUsrID, "voters" => array($own_registration), "response" => $voteChoices));
 
     $ballotsJSON = json_decode($ballots_raw, true);
 
