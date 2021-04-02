a2ba0ff775ab5ef98608df0c5340b2bb307a9af5
toufic.talha <toufic.talha@u-psud.fr>
2021-04-02 21:31:16 +0200

    Some style shit just for fun

--- js/home.js
+++ js/home.js
@@ -3,9 +3,9 @@ function displayBallots(){
       dataType: "json",
       url: "../php/searchBallot.php",
     }).done(function(obj) {
-      //console.log(obj)
-      for (bal of obj)
-          $("#list").append("<tr><td>"+bal.title+"</td><td>"+bal.promoter+"</td><td>"+bal.pr+"%</td><td><button type='button'>Check</button></td></tr>");
+      for (bal of obj){
+          $("#list").append("<tr><td>"+bal.title+"</td><td>"+bal.promoter+"</td><td>"+bal.pr+"</td><td><button type='button' id='checkBtn'>Check</button></td></tr>");
+      }
 
     }).fail(function(e) {
       console.log(e);
