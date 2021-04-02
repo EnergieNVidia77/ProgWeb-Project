76a0834c1051792d2fb97280f4f3d9fb40c62c07
toufic.talha <toufic.talha@u-psud.fr>
2021-04-02 20:41:08 +0200

    Implementation of creating ballots 90% done !

--- js/home.js
+++ js/home.js
@@ -13,12 +13,14 @@ function displayBallots(){
     });
 }
 
+//Begin the vote creation
+
 function CreateBallotPageSetup () {
   $(".ballots").empty();
   $(".ballots").append("<h1>Create Ballot :</h1>");
 
   $("#BallotSetUp").remove();
-  $(".content .menu").prepend("<button type='button' id='goBackBtn'>Go back</button>")
+  $(".content .menu").prepend("<button type='button' id='goBackBtn' onclick='homePageSetup()'>Go back</button>")
 
   $(".ballots").append("<div class='optionSection'></div>");
 
@@ -34,10 +36,12 @@ function CreateBallotPageSetup () {
 
   $(".optionSection").append("<div id='BtnWrapper'></div>");
 
-  $("#BtnWrapper").append("<button type='button' id='confirmVoteBtn'>Confirm</button>");
+  $("#BtnWrapper").append("<button type='button' id='nextStepBtn' onclick='listPageSetup()'>Next step</button>");
   $("#BtnWrapper").append("<button type='button' id='addChoiceBtn' onclick='addOptionItem()'>Add choice</button>");
 
 }
+
+//Add choices to the vote
 
 function addOptionItem(){
   let nbChoice = document.getElementById('optionItem').children.length + 1;
@@ -49,6 +53,69 @@ function addOptionItem(){
   }
   if(nbChoice == 8){
     $(".optionItem").append("<label>Choix " + nbChoice + ": </label> <input type='text' id='optionItem"+ nbChoice +"' placeholder='Choix "+ nbChoice +"'>");
+
+    //Disabeling button
     $('#addChoiceBtn').prop('disabled', true);
   }
+}
+
+//Go back to the home page from the vote creation screen
+
+function homePageSetup() {
+    $(".content").empty();
+
+    $(".content").append("<div class='menu'></div>");
+    $(".menu").append("<button id='BallotSetUp' type='button' onclick='CreateBallotPageSetup()'>Create ballot</button>");
+    $(".menu").append("<button class='logout-btn' onclick='logOut()'>LOGOUT</button>");
+
+    $(".content").append("<div class='ballots'></div>");
+    $('.ballots').append("<h1>Ballots</h1>");
+    $('.ballots').append(" <table id ='list'></table>");
+    $('#list').append("<tr><th>Title</th><th>Promoter</th><th>Participation rate</th></tr>");
+    displayBallots();
+}
+
+function logOut() {
+  console.log("You've logged out !");
+  window.location = "./login.html";
+}
+
+
+
+function listPageSetup(){
+  let nbChoice = document.getElementById('optionItem').children.length + 1;
+  if(nbChoice > 1){
+    nbChoice = Math.round(nbChoice / 2) - 1;
+  }
+  let allOptionItem = document.getElementById('optionItem').children;
+
+  let arrayOfChoice = [];
+
+  for (input of allOptionItem){
+    if(input.tagName === "INPUT"){
+      let hTag = "#";
+      let id_raw = String(input.id);
+      let id = hTag.concat(id_raw);
+      let val = $(id).val();
+      arrayOfChoice.push(val);
+    }
+  }
+
+  let voteTitle = $('#voteTitle').val();
+  let voteQuestion = $('#voteQuestion').val();
+
+  $.ajax({
+    method: "POST",
+    dataType: "json",
+    url: "../php/createBallot.php",
+    data: {
+      "voteTitle": voteTitle,
+      "voteQuestion": voteQuestion,
+      "arrayOfChoice": arrayOfChoice
+    }
+  }).done(function (e) {
+    console.log(e);
+  }).fail(function (e) {
+    console.log("Error");
+  });
 }
--- php/createBallot.php
+++ php/createBallot.php
@@ -1,12 +1,23 @@
 <?php
 
     $CurrentUsrID = $_COOKIE["CurrentUsrID"];
-    
-    $ballots_raw = file_get_contents("../logs/ballots.json");
+    $voteTitle = $_POST["voteTitle"];
+    $voteQuestion = $_POST["voteQuestion"];
+    $voteChoices = $_POST["arrayOfChoice"];
+    $voteID = uniqid();
 
-    $ballotsJSON = json_decode($ballots_raw, true);
+    $array_of_votes_raw = file_get_contents("../logs/ballots.json");
+    $array_of_votes = json_decode($array_of_votes_raw, true);
 
+    $vote = array(0 => array("voteID" => $voteID, "title" => $voteTitle, "promoter" => $CurrentUsrID, "voters" => array(), "response" => $voteChoices));
+
+    $new_array_of_votes = array_merge($array_of_votes['votes'], $vote);
+
+    $final_array = array("votes" => $new_array_of_votes);
     
+    $new_array_of_votes_JSON = json_encode($final_array, JSON_PRETTY_PRINT);
+
+    $test = file_put_contents("../logs/ballots.json", $new_array_of_votes_JSON);
 
 
 ?>
