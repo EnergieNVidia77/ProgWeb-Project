cb153a2c841af6010cafe0eaf89a61ed03cfc7b1
toufic.talha <toufic.talha@u-psud.fr>
2021-04-02 20:47:59 +0200

    bug fix on creating ballot feature

--- php/createBallot.php
+++ php/createBallot.php
@@ -9,7 +9,9 @@
     $array_of_votes_raw = file_get_contents("../logs/ballots.json");
     $array_of_votes = json_decode($array_of_votes_raw, true);
 
-    $vote = array(0 => array("voteID" => $voteID, "title" => $voteTitle, "promoter" => $CurrentUsrID, "voters" => array(), "response" => $voteChoices));
+    $own_registration = array("userID" => $CurrentUsrID, "vote" => "NULL", "nbVote" => 1, "procuration" => "NULL");
+
+    $vote = array(0 => array("voteID" => $voteID, "title" => $voteTitle, "promoter" => $CurrentUsrID, "voters" => array($own_registration), "response" => $voteChoices));
 
     $new_array_of_votes = array_merge($array_of_votes['votes'], $vote);
 
